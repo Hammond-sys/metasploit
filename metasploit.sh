97579c9122e57d199b8b50326f44194b3ea63210
hammmomd-sys <53830640+Hammond-sys@users.noreply.github.com>
2019-08-10 19:26:38 +0700

    Add files via upload

---
+++ metasploit.sh
@@ -0,0 +1,40 @@
+#hanya untuk metasploit
+apt update
+apt upgrade
+apt install ruby
+pkg install git
+pkg install python python2
+gem install lolcat
+sleep 02
+clear
+echo "sedang melakukan penginstalan bahan"|lolcat
+echo "silahkan tunggu ya bang/kak"|lolcat
+echo "Jangan lupa subscribe channel Hammond cyber "|lolcat
+sleep 05
+clear
+
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+clear
+echo "Tunggu sedang melakukan penginstallan metasploit"|lolcat
+sleep 02
+clear
+pkg install metasploit
+echo "sedang membuka metasploit"|lolcat
+                                                                                                     
+                                         
+                      
+               
+    
+ 
+                                 
+                                             
+                         
+                  
+            
+
+
+
+
+msfconsole
